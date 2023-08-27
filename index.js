9632b644683bd0f3471aa21604123e1e302e7f1a
Haris Arapovic <hare.arapovic@gmail.com>
2023-08-27 21:12:18 +0200

    Database setup connection

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import mysql from "mysql2";
 
 const app = express();
 dotenv.config();
 
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.DBPASSWORD,
+});
+
+conn.connect((error) => {
+  if (error) {
+    console.log("Database connection error");
+  } else {
+    console.log("Database successfully connected");
+  }
+});
+
+conn.once("open", () => {
+  console.log("Connected to the database");
+});
+
 const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`) )
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
