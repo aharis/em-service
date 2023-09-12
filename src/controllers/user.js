ab5dc4a5c523d80859733d2bf5983c7f03e2b936
Haris Arapovic <hare.arapovic@gmail.com>
2023-09-12 21:47:49 +0200

    Implement JWT for secure user authentication

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,4 +1,5 @@
 import conn from "../config/db.js";
+import jwt from "jsonwebtoken";
 
 export const loginUser = (req, res) => {
   const sql = "SELECT * FROM users Where email = ? AND password = ?";
@@ -6,9 +7,19 @@ export const loginUser = (req, res) => {
     if (error)
       return res.status(500).json({ message: "Internal server error" });
     if (result.length === 0) {
-      return res.status(400).json({ message: "User not found" });
+      return res
+        .status(400)
+        .json({ message: "User not found or invalid user credentials" });
     } else {
-      return res.status(200).json({ message: "Success", result: result });
+      const userId = result[0].userId;
+      const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
+        expiresIn: "1d",
+      });
+      const response = {
+        role: result[0].roles,
+        token: token,
+      };
+      return res.status(200).json({ message: "User loged", result: response });
     }
   });
 };
