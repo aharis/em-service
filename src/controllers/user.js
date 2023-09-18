134554c522a7b5bedc6e18509b30bed2c2f71f55
Haris Arapovic <hare.arapovic@gmail.com>
2023-09-18 21:50:37 +0200

    Get number of admins

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -16,6 +16,7 @@ export const loginUser = (req, res) => {
         expiresIn: "1d",
       });
       const response = {
+        userId: result[0].userId,
         role: result[0].roles,
         token: token,
       };
@@ -27,6 +28,20 @@ export const loginUser = (req, res) => {
 export const getUsers = (req, res) => {
   const sql = "SELECT * FROM users";
   conn.query(sql, (error, result) => {
+    if (error)
+      return res.status(500).json({ message: "Internal server error" });
+    if (result.length === 0) {
+      return res.status(400).json({ message: "Users not found" });
+    } else {
+      return res.status(200).json({ message: "Success", result: result });
+    }
+  });
+};
+
+export const getUser = (req, res) => {
+  const userId = req.params.id;
+  const sql = "SELECT * FROM users WHERE userId = ?";
+  conn.query(sql, [userId], (error, result) => {
     if (error)
       return res.status(500).json({ message: "Internal server error" });
     if (result.length === 0) {
@@ -42,8 +57,23 @@ export const deleteUser = (req, res) => {
   const sql = "DELETE FROM users WHERE userId = ?";
   conn.query(sql, [userId], (error, result) => {
     if (error) {
-      return res.status(500).json({ message: "Error in SQL query", details: error });
+      return res
+        .status(500)
+        .json({ message: "Error in SQL query", details: error });
     }
     return res.status(200).json({ message: "User successfully deleted" });
+  });
+};
+
+export const adminCount = (req, res) => {
+  const sql = "SELECT count(userId) as admin FROM users";
+  conn.query(sql, (error, result) => {
+    if (error) {
+      return res.status(500).json({ message: "Internal server error" });
+    } else if (result.length === 0) {
+      return res.status(400).json({ message: "User not found" });
+    } else {
+      return res.status(200).json({ message: "Success", result: result });
+    }
   });
 };
--- src/routes/r1/routes.js
+++ src/routes/r1/routes.js
@@ -1,5 +1,11 @@
 import { Router } from "express";
-import { loginUser, getUsers, deleteUser } from "../../controllers/user.js";
+import {
+  loginUser,
+  getUsers,
+  deleteUser,
+  getUser,
+  adminCount,
+} from "../../controllers/user.js";
 import {
   addEmployee,
   getEmployees,
@@ -14,11 +20,13 @@ const router = Router();
 
 router.post("/login", loginUser, authRoutes);
 router.get("/users", getUsers);
+router.get("/users/:id", getUser);
 router.delete("/users/:id", deleteUser);
 router.post("/employee/create", uploadImage, addEmployee);
 router.get("/employee", getEmployees);
 router.delete("/employee/:id", deleteEmployee);
 router.get("/employee/:id", getEmployee);
 router.post("/employee/:id", uploadImage, editEmployee);
+router.get("/adminCount", adminCount, authRoutes);
 
 export default router;
